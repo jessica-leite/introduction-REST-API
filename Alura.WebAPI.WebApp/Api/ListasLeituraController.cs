e0fa15e1f176653f3fdaf78e53db1bb36154e001
Jessica <jessica@email.com>
2020-04-09 14:29:06 +0000

    ListasLeituraController added

---
+++ Alura.WebAPI.WebApp/Api/ListasLeituraController.cs
@@ -0,0 +1,50 @@
+﻿using Alura.ListaLeitura.Modelos;
+using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Lista = Alura.ListaLeitura.Modelos.ListaLeitura;
+
+namespace Alura.WebAPI.WebApp.Api
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ListasLeituraController : ControllerBase
+    {
+        private readonly IRepository<Livro> _repo;
+
+        public ListasLeituraController(IRepository<Livro> repository)
+        {
+            _repo = repository;
+        }
+
+        private Lista CriarLista(TipoListaLeitura tipo)
+        {
+            return new Lista
+            {
+                Tipo = tipo.ParaString(),
+                Livros = _repo.All.Where(l => l.Lista == tipo).Select(l => l.ToApi())
+            };
+        }
+
+        [HttpGet]
+        public IActionResult TodasListas()
+        {
+            Lista paraLer = CriarLista(TipoListaLeitura.ParaLer);
+            Lista lendo = CriarLista(TipoListaLeitura.Lendo);
+            Lista lidos = CriarLista(TipoListaLeitura.Lidos);
+
+            var colecao = new List<Lista>{ paraLer, lendo, lidos };
+
+            return Ok(colecao);
+        }
+
+        [HttpGet("{tipo}")]
+        public IActionResult Recuperar(TipoListaLeitura tipo)
+        {
+            Lista lista = CriarLista(tipo);
+
+            return Ok(lista);
+        }
+    }
+}
